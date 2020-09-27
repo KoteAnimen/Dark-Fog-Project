b4c9a86a89328e8bde0da955656ecdac89528166
Kote\Kote_Animen <sverh.il@yandex.ru>
2020-09-27 17:12:41 +0300

    Доработать систему смерти и оживления врага

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public bool isDead;
+	public float health;//жизни
+	public float strength;//сила
+	public float agility;//ловкость
+	public float stamina;//выносливость
+	public float intelligence;//интеллект
+	public float mana;// кол-во маны	
+	private Vector3 startPosition = new Vector3();
+	void Start()
+    {
+        startPosition = transform.position;
+		SetRigidbodyState(true);
+		SetColliderState(false);
+		
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        EnemyDead(isDead);
+		AliveEnemy(isDead);
+    }
+	
+	public void EnemyDead(bool dead)
+	{
+		if(dead)
+		{
+			GetComponent<Animator>().enabled = false;
+			SetRigidbodyState(false);
+			SetColliderState(true);
+		}
+	}
+	public void AliveEnemy(bool dead)
+	{
+		if(!dead)
+		{
+			GetComponent<Animator>().enabled = true;
+			SetRigidbodyState(true);
+			SetColliderState(false);
+		}
+	}
+	void SetRigidbodyState(bool state)
+	{
+		Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();// через похожую лабуду найдем начальную позицию всех костей, чтоб потом их на место поставить:D
+		foreach(Rigidbody rigidbody in rigidbodies)
+		{
+			rigidbody.isKinematic = state;
+		}
+		GetComponent<Rigidbody>().isKinematic = !state;
+	}
+	
+	void SetColliderState(bool state)
+	{
+		Collider[] colliders = GetComponentsInChildren<Collider>();
+		foreach(Collider collider in colliders)
+		{
+			collider.enabled = state;
+		}
+		GetComponent<Collider>().enabled = !state;
+	}
+}
